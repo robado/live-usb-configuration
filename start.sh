c885f56e3f0cb771e5394de3ddb81adca4e1fe69
Robert Ridal <ridal.robert@gmail.com>
2018-05-05 18:33:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo -e "\e[38;5;82mEnabling universe, updating and upgrading.....\e[38;5;82m"
+echo -e "\e[38;5mEnabling universe, updating and upgrading.....\e[38;5m"
 sudo add-apt-repository universe
-sudo apt-get -y update && sudo -y apt-get upgrade
+sudo apt-get -y update && sudo apt-get -y upgrade
 
-echo -e "\e[38;5;82mInstalling git and salt-master\e[38m"
+echo -e "\e[38;5mInstalling git and salt-master\e[38;5m"
 sudo apt-get -y install git salt-master
 echo -e "\e[38;5;82mCloning git repo\e[38m"
 git clone https://github.com/robado/live-usb-configuration
@@ -11,9 +11,9 @@ git clone https://github.com/robado/live-usb-configuration
 echo -e "\e[0;0mGoing to live-usb-configuration\e[0;0m"
 cd live-usb-configuration/
 
-echo -e "\e[38;5;82mInstalling git repo modules\e[38m"
+echo -e "\e[38;5mInstalling git repo modules\e[38;5m"
 ./highstate.sh && cd
-echo -e "\e[38;5;82mOpening terminals and gtop in one of them\e[38m"
+echo -e "\e[38;5mOpening terminals and gtop in one of them\e[38;5m"
 ./terminal.sh
 
 echo -e "Exiting......"
