e0695e2a1a4f38ef312c70d8c381b6f6a897077f
Robert Ridal <ridal.robert@gmail.com>
2018-05-05 18:38:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,6 @@ echo -e "\e[38;5mInstalling git and salt-master\e[38;5m"
 sudo apt-get -y install git salt-master
 echo -e "\e[38;5;82mCloning git repo\e[38m"
 git clone https://github.com/robado/live-usb-configuration
-
-echo -e "\e[0;0mGoing to live-usb-configuration\e[0;0m"
 cd live-usb-configuration/
 
 echo -e "\e[38;5mInstalling git repo modules\e[38;5m"
